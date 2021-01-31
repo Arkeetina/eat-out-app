c370c49d9792ce1169859571b9217b1e6119546f
Katerina <ekaterinabez@gmail.com>
2021-01-31 19:06:00 +0100

    updated versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   //   removeDeprecatedGapUtilities: true,
   //   purgeLayersByDefault: true,
   // },
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
