c69678b7fabcc9daa37e9cc24c776e687cca1225
Katerina <ekaterinabez@gmail.com>
2021-01-31 18:35:09 +0100

    updated paths

--- src/schema.ts
+++ src/schema.ts
@@ -52,7 +52,7 @@ export const schema = makeSchema({
     }),
   ],
   outputs: {
-    schema: path.join(process.cwd(), "pages", "api", "graphql.ts"),
+    schema: path.join(process.cwd(), "schema.graphql"),
     typegen: path.join(process.cwd(), "nexus.ts"),
   },
   sourceTypes: {
